c2b90ad5aca79332805d01be388a60e71789e9c3
Ma77i <mattiasuru@gmail.com>
2021-07-05 18:52:25 -0300

    agregado js

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
-AOS.init({
-    duration: 1200,
-  })
+
+
+
+  class Platos {
+    constructor ({id, nombre, precio, descripcion}){
+      
+      this.id = parseInt(id),
+      this.nombre = nombre,
+      this.precio = parseInt(precio),
+      this.descripcion = descripcion
+
+    }
+
+  }
+
+
+
+let DATOS = [];
+
+let detalles = [];
+
+
+$(document).ready(function () {
+
+  $.getJSON("data.json", function (data, respuesta) {
+      if (respuesta === "success") {
+        DATOS =  data
+      }
+
+      for (const dato of DATOS) {
+        detalles.push(new Platos(dato));
+        
+      }
+
+      for (const detalle of detalles) {
+        $(".caja").append(`<p class="plato">${detalle.nombre}</p>
+                        <span class="precio">$${detalle.precio}</span>
+                        <span class="descripcion">${detalle.descripcion}</span>
+                        <p></p>`);
+      }
+    }
+  );
+});
+
+
+
+
+
+
+
+
